606305b0ddeff1ea965fcccc3eb274433f098935
Prince Mishra <prince.mishra1384@gmail.com>
2022-12-19 23:10:55 +0530

    made apis and tested to increase cover to 94%

---
+++ core/models/assignments.py
@@ -0,0 +1,97 @@
+import enum
+from core import db
+from core.apis.decorators import Principal
+from core.libs import helpers, assertions
+from core.models.teachers import Teacher
+from core.models.students import Student
+from sqlalchemy.types import Enum as BaseEnum
+
+
+class GradeEnum(str, enum.Enum):
+    A = 'A'
+    B = 'B'
+    C = 'C'
+    D = 'D'
+
+
+class AssignmentStateEnum(str, enum.Enum):
+    DRAFT = 'DRAFT'
+    SUBMITTED = 'SUBMITTED'
+    GRADED = 'GRADED'
+
+
+class Assignment(db.Model):
+    __tablename__ = 'assignments'
+    id = db.Column(db.Integer, db.Sequence('assignments_id_seq'), primary_key=True)
+    student_id = db.Column(db.Integer, db.ForeignKey(Student.id), nullable=False)
+    teacher_id = db.Column(db.Integer, db.ForeignKey(Teacher.id), nullable=True)
+    content = db.Column(db.Text)
+    grade = db.Column(BaseEnum(GradeEnum))
+    state = db.Column(BaseEnum(AssignmentStateEnum), default=AssignmentStateEnum.DRAFT, nullable=False)
+    created_at = db.Column(db.TIMESTAMP(timezone=True), default=helpers.get_utc_now, nullable=False)
+    updated_at = db.Column(db.TIMESTAMP(timezone=True), default=helpers.get_utc_now, nullable=False, onupdate=helpers.get_utc_now)
+
+    def __repr__(self):
+        return '<Assignment %r>' % self.id
+
+    @classmethod
+    def filter(cls, *criterion):
+        db_query = db.session.query(cls)
+        return db_query.filter(*criterion)
+
+    @classmethod
+    def get_by_id(cls, _id):
+        return cls.filter(cls.id == _id).first()
+
+    @classmethod
+    def upsert(cls, assignment_new: 'Assignment'):
+        if assignment_new.id is not None:
+            assignment = Assignment.get_by_id(assignment_new.id)
+            assertions.assert_found(assignment, 'No assignment with this id was found')
+            assertions.assert_valid(assignment.state == AssignmentStateEnum.DRAFT,
+                                    'only assignment in draft state can be edited')
+
+            assignment.content = assignment_new.content
+        else:
+            assignment = assignment_new
+            db.session.add(assignment_new)
+
+        db.session.flush()
+        return assignment
+
+    @classmethod
+    def submit(cls, _id, teacher_id, principal: Principal):
+        assignment = Assignment.get_by_id(_id)
+        assertions.assert_found(assignment, 'No assignment with this id was found')
+        assertions.assert_valid(assignment.student_id == principal.student_id, 'This assignment belongs to some other student')
+        assertions.assert_valid(assignment.state == AssignmentStateEnum.DRAFT, 'only a draft assignment can be submitted')
+        assertions.assert_valid(assignment.content is not None, 'assignment with empty content cannot be submitted')
+        assertions.assert_valid(assignment.teacher_id is None, 'only a draft assignment can be submitted')
+
+        assignment.teacher_id = teacher_id
+        assignment.state = AssignmentStateEnum.SUBMITTED
+        db.session.flush()
+
+        return assignment
+
+    @classmethod
+    def gradeAssignment(cls, _id, gradeOfAssignment, principal: Principal):
+        assignment = Assignment.get_by_id(_id)
+        assertions.assert_found(assignment, 'No assignment with this id was found')
+        assertions.assert_valid(assignment.teacher_id == principal.teacher_id, 'This assignment belongs to some other teacher')
+        assertions.assert_valid(gradeOfAssignment in ['A', 'B', 'C', 'D'], 'assignment with empty content cannot be submitted')
+
+        assignment.grade = gradeOfAssignment
+        assignment.state = AssignmentStateEnum.GRADED
+        db.session.flush()
+
+        return assignment
+
+
+    @classmethod
+    def get_assignments_by_student(cls, student_id):
+        return cls.filter(cls.student_id == student_id).all()
+
+    @classmethod
+    def get_assignments_by_teacher(cls, teacher_id):
+        return cls.filter(cls.teacher_id == teacher_id).all()
